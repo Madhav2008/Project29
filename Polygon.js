50d64a963acac743e3ea9a779fa724d2702cedb6
Madhav2008 <72864817+Madhav2008@users.noreply.github.com>
2020-12-01 15:22:33 +0530

    Add files via upload

---
+++ Polygon.js
@@ -0,0 +1,21 @@
+class Polygon {
+    constructor(x, y, radius) {
+        var options = {
+            restitution: 0.8,
+            friction: 1.0,
+            density: 1.0,
+            isStatic: false
+        }
+        this.image = loadImage("polygon.png");
+        this.body = Bodies.circle(x, y, radius, options);
+        this.radius = radius;
+        World.add(world, this.body);
+    }
+    display() {
+        // image(this.image, 0, 0, this.width, this.height);
+        stroke("red");
+        strokeWeight(8);
+        ellipseMode(RADIUS);
+        ellipse(this.body.position.x, this.body.position.y, this.radius);
+    }
+}
